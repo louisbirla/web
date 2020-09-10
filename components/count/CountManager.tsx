97d1dc1501d5a61559ff2f77f8a171c9253425a9
Louis Birla <louis@birla.io>
2020-09-10 01:57:06 +0000

    Fix things

---
+++ components/count/CountManager.tsx
@@ -0,0 +1,38 @@
+import { CountCrement } from './CountCrement'
+import { CountDisplay } from './CountDisplay'
+import { useQuery, useMutation, gql } from '@apollo/client'
+import { UpdateCount } from './gql/UpdateCount'
+import { CountQuery } from './gql/CountQuery'
+
+export const COUNT = gql`
+  query CountQuery {
+    count
+  }
+`
+
+export const UPDATE_COUNT = gql`
+  mutation UpdateCount($by: Int!) {
+    updateCount(by: $by)
+  }
+`
+export const CountManager = () => {
+  const { data, refetch } = useQuery<CountQuery>(COUNT, {
+    pollInterval: 1000,
+  })
+  const [updateCount] = useMutation<UpdateCount>(UPDATE_COUNT)
+
+  const handleCrement = (by: number) => {
+    updateCount({
+      variables: { by },
+    })
+    refetch()
+  }
+
+  return (
+    <>
+      <CountCrement by={1} updateCount={handleCrement} />
+      <CountDisplay count={data?.count} />
+      <CountCrement by={-1} updateCount={handleCrement} />
+    </>
+  )
+}
--- pages/count.tsx
+++ pages/count.tsx
@@ -1,42 +1,12 @@
 import { Layout } from '../components/Layout'
-import { CountDisplay } from '../components/count/CountDisplay'
 import { Center } from '@chakra-ui/core'
-import { CountCrement } from '../components/count/CountCrement'
-import { gql, useQuery, useMutation } from '@apollo/client'
-import { CountQuery } from './gql/CountQuery'
-import { UpdateCount } from './gql/UpdateCount'
-
-export const COUNT = gql`
-  query CountQuery {
-    count
-  }
-`
-
-export const UPDATE_COUNT = gql`
-  mutation UpdateCount($by: Int!) {
-    updateCount(by: $by)
-  }
-`
+import { CountManager } from '../components/count/CountManager'
 
 const IndexPage = () => {
-  const { data, refetch } = useQuery<CountQuery>(COUNT, {
-    pollInterval: 1000,
-  })
-  const [updateCount] = useMutation<UpdateCount>(UPDATE_COUNT)
-
-  const handleCrement = (by: number) => {
-    updateCount({
-      variables: { by },
-    })
-    refetch()
-  }
-
   return (
     <Layout title='Count | Loop'>
       <Center>
-        <CountCrement by={1} updateCount={handleCrement} />
-        <CountDisplay count={data?.count} />
-        <CountCrement by={-1} updateCount={handleCrement} />
+        <CountManager />
       </Center>
     </Layout>
   )
