cbad27b5fffc201ceae23cbb727d38bae3fbd4cc
Louis Birla <louis@birla.io>
2020-09-12 19:04:25 +0000

    SITE-15 Hide logo instead of showing alt text when loading

--- components/LogoImage.tsx
+++ components/LogoImage.tsx
@@ -1,8 +1,16 @@
 import { ImageProps, Image, useColorMode } from '@chakra-ui/core'
 import { dark } from '../utils/theme/dark'
+import { useState } from 'react'
 
+/** The logo, which changes dynamically based on color mode */
 export const LogoImage: React.FC<ImageProps> = (props) => {
+  // The image shows an ugly alt before loading, so don't show before loaded
+  const [isLoading, setIsLoading] = useState(true)
+  // Choose the right version of the logo
   const { colorMode } = useColorMode()
   const src = dark({ colorMode }) ? '/donuts/white-donut.svg' : '/donuts/black-donut.svg'
-  return <Image src={src} alt='Loop Logo' {...props} />
+
+  return (
+      <Image opacity={isLoading ? 0 : 1} onLoad={() => setIsLoading(false)} src={src} alt='Loop Logo' {...props} />
+  )
 }
