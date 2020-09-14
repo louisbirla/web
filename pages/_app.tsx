fbe16433d2a3a436d66086b49024f0034f771a77
Louis Birla <louis@birla.io>
2020-09-14 18:06:59 +0000

    Use yarn 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,13 @@ import { Metadata } from '../components/Metadata'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { init } from '@sentry/react'
 
-init({
-  dsn: 'https://190272e39fa74484a58c821b9ed30555@o336780.ingest.sentry.io/5420493',
-})
+const prod = process.env.NODE_ENV === 'production'
+
+if (prod) {
+  init({
+    dsn: 'https://190272e39fa74484a58c821b9ed30555@o336780.ingest.sentry.io/5420493',
+  })
+}
 
 const client = new ApolloClient({
   uri: 'https://api.loop.page',
@@ -18,7 +22,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ApolloProvider client={client}>
       <ChakraProvider resetCSS theme={customTheme}>
-        <Metadata />
+        <Metadata ga={prod} />
         <Component {...pageProps} />
       </ChakraProvider>
     </ApolloProvider>
