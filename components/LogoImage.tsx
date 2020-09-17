693e94c57a296c958a1cc1c8cc32ecf8f0d6ff04
nilaeus <nilaeus@users.noreply.github.com>
2020-09-17 16:54:21 +0000

    Automatic formatting

--- components/LogoImage.tsx
+++ components/LogoImage.tsx
@@ -3,7 +3,7 @@ import { dark } from '../utils/theme/dark'
 import { useState, useEffect } from 'react'
 
 /** The logo, which changes dynamically based on color mode */
-export const LogoImage: React.FC<ImageProps & {skipLoading?: boolean}> = (props) => {
+export const LogoImage: React.FC<ImageProps & { skipLoading?: boolean }> = (props) => {
   // The image shows an ugly alt before loading, so don't show before loaded
   const [isLoading, setIsLoading] = useState(true)
   // Choose the right version of the logo
@@ -11,10 +11,19 @@ export const LogoImage: React.FC<ImageProps & {skipLoading?: boolean}> = (props)
   const src = dark({ colorMode }) ? '/donuts/white-donut.svg' : '/donuts/black-donut.svg'
   const onLoad = () => setIsLoading(false)
   useEffect(() => {
-    if(props.skipLoading) {
+    if (props.skipLoading) {
       onLoad()
     }
   }, [onLoad, props.skipLoading])
 
-  return <Image opacity={isLoading ? 0 : 1} title={isLoading ? 'logo loading' : 'loop logo'} onLoad={onLoad} src={src} alt='Loop Logo' {...props} />
+  return (
+    <Image
+      opacity={isLoading ? 0 : 1}
+      title={isLoading ? 'logo loading' : 'loop logo'}
+      onLoad={onLoad}
+      src={src}
+      alt='Loop Logo'
+      {...props}
+    />
+  )
 }
--- tests/components/LogoImage.test.tsx
+++ tests/components/LogoImage.test.tsx
@@ -1,6 +1,4 @@
-import {
-  render,
-} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { LogoImage } from '../../components/LogoImage'
 import '@testing-library/jest-dom'
 import { jukeError } from '../__helpers'
@@ -8,7 +6,11 @@ import { jukeError } from '../__helpers'
 describe('logo image', () => {
   test('image loading', async () => {
     jukeError(() => {
-      const image = render(<><LogoImage skipLoading /></>)
+      const image = render(
+        <>
+          <LogoImage skipLoading />
+        </>,
+      )
       expect(image).toBeTruthy()
     })
   })
