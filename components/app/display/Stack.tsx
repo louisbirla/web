daff522f7f47d662669dbf7a7bf35959d44313a5
Louis Birla <louis.birla@gmail.com>
2021-01-31 15:24:46 -0600

    fix(display): :bug: use the direction arg from stack components

--- components/app/display/Stack.tsx
+++ components/app/display/Stack.tsx
@@ -1,9 +1,17 @@
-import { Heading, Stack, Text } from "@chakra-ui/core"
+import { Flex, HStack, Stack } from "@chakra-ui/core"
 import { StackArgs } from "display-api"
 import { ComponentDelegate } from "./ComponentDelegate"
 
 export const StackComponent: React.FC<StackArgs> = ({ direction, items }) => {
-	return <Stack>
-		{items.map(({ component }) => <ComponentDelegate key={JSON.stringify(component)} component={component} />)}
-	</Stack>
+	const content = items.map(({ component }) => (
+		<ComponentDelegate key={JSON.stringify(component)} component={component} />
+	))
+	switch (direction) {
+		case "vertical":
+			return <Stack>{content}</Stack>
+		case "horizontal":
+			return <HStack>{content}</HStack>
+		case "fit":
+			return <Flex flexWrap='wrap'>{content}</Flex>
+	}
 }
