43d0e99ab678f6b768bdbdee977c2a7af64d202b
Louis Birla <louis.birla@gmail.com>
2021-02-11 14:45:48 -0600

    fix: use renamed icon names

--- components/display/components/Card.tsx
+++ components/display/components/Card.tsx
@@ -1,8 +1,8 @@
 import { Box, Heading, HStack } from "@chakra-ui/react"
-import { CardArgs, CardIcon } from "display-api"
+import { CardArgs } from "display-api"
 import Link from "next/link"
-import { CheckCircle, Folder, IconProps, MessageSquare, Type, Box as BoxIcon, Rss } from "react-feather"
 import { ComponentDelegate } from "../ComponentDelegate"
+import { IconComponent } from "./Icon"
 
 export const CardComponent: React.FC<CardArgs> = ({ header, color, content }) => {
 	color = color || "#5D80FE"
@@ -19,7 +19,7 @@ export const CardComponent: React.FC<CardArgs> = ({ header, color, content }) =>
 		cardHeader = (
 			<Box pb={2}>
 				<HStack>
-					<CardIconComponent icon={header.icon} color={color} />
+					<IconComponent name={header.icon} color={color} />
 					{wrapLink(
 						<Heading pl={1} size='md' fontWeight='bold'>
 							{header.title}
@@ -48,24 +48,4 @@ export const CardComponent: React.FC<CardArgs> = ({ header, color, content }) =>
 			</Box>
 		</Box>
 	)
-}
-
-const CardIconComponent: React.FC<{ icon?: CardIcon; color: string }> = ({ icon, color }) => {
-	let commonProps: IconProps = { color, size: 23 }
-	switch (icon) {
-		case "Folder":
-			return <Folder {...commonProps} />
-		case "TaskComplete":
-			return <CheckCircle {...commonProps} />
-		case "Message":
-			return <MessageSquare {...commonProps} style={{ marginTop: "4px" }} />
-		case "Box":
-			return <BoxIcon {...commonProps} />
-		case "Type":
-			return <Type {...commonProps} />
-		case "Feed":
-			return <Rss {...commonProps} />
-		default:
-			return <BoxIcon {...commonProps} />
-	}
 }
