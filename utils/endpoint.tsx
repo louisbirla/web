1ef84c542de3e23d48a2340dfd1fabd7519c8099
Louis Birla <louis.birla@gmail.com>
2021-02-28 18:36:54 -0600

    fix: fix endpoint

--- utils/endpoint.tsx
+++ utils/endpoint.tsx
@@ -1,3 +1,2 @@
-// export const api_url = "https://api.loop.page"
-export const api_url = "https://api-pr-151.onrender.com"
+export const api_url = "https://api.loop.page"
 // export const api_url = "http://localhost:4000";
