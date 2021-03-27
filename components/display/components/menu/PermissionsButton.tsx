f796f5875b937a47f55d037ccf6fb97e57a31d9c
Salman Maredia <salmanaly1492@gmail.com>
2021-03-27 17:28:02 -0400

    [WIP] User permissions

--- components/display/components/menu/PermissionsButton.tsx
+++ components/display/components/menu/PermissionsButton.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@chakra-ui/button"
 import { useDisclosure } from "@chakra-ui/hooks"
-import { Flex, Text } from "@chakra-ui/layout"
+import { Box, Flex, Heading, HStack, Stack, Text } from "@chakra-ui/layout"
 import {
 	Drawer,
 	DrawerBody,
@@ -11,10 +10,12 @@ import {
 	DrawerOverlay,
 } from "@chakra-ui/modal"
 import { Switch } from "@chakra-ui/switch"
-import { RefObject, useRef, useState } from "react"
-import { gql, useMutation } from "urql"
-import { Icon, Tooltip } from "@chakra-ui/react"
+import { RefObject, useEffect, useRef, useState } from "react"
+import { gql, useMutation, useQuery } from "urql"
+import { Avatar, Icon, Select, Tooltip, Button } from "@chakra-ui/react"
 import { Info } from "react-feather"
+import { UserArray } from "../../../search/UserSearchResults"
+import { SearchComponentWrapper } from "../Search"
 
 export const SetVisibilityQuery = gql`
 	mutation($blockId: Int!, $public: Boolean!) {
@@ -23,21 +24,172 @@ export const SetVisibilityQuery = gql`
 		}
 	}
 `
+
+export const GetUserPermissions = gql`
+	query($blockId: Int!) {
+		blockById(id: $blockId) {
+			full: permFull(level: FULL) {
+				id
+				username
+				displayName
+			}
+			edit: permFull(level: EDIT) {
+				id
+				username
+				displayName
+			}
+			view: permFull(level: VIEW) {
+				id
+				username
+				displayName
+			}
+		}
+	}
+`
+export const SetUserPermissions = gql`
+	mutation($full: [Int!]!, $edit: [Int!]!, $view: [Int!]!, $blockId: Int!) {
+		setPerms(permFull: $full, permEdit: $edit, permView: $view, blockId: $blockId) {
+			full: permFull(level: FULL) {
+				id
+				username
+				displayName
+			}
+			edit: permFull(level: EDIT) {
+				id
+				username
+				displayName
+			}
+			view: permFull(level: VIEW) {
+				id
+				username
+				displayName
+			}
+		}
+	}
+`
+
 export type SetVisibilityArgsVars = { blockId: number; public: boolean }
+export type SetUserPerissionsVars = { full: Array<number>, edit: Array<number>, view: Array<number>, blockId: number }
+export type GetUserPerissionsVars = { blockId: number }
+export type UserPermission = { full: UserArray, edit: UserArray, view: UserArray }
+export type GetUserPermissionResult = { blockById: UserPermission }
+export enum PermissionType {
+	full = 0,
+	edit = 1,
+	view = 2
+}
+
 
 export const usePermissionButton = (blockId: number, pub: boolean): [JSX.Element, () => void, RefObject<any>] => {
 	const { isOpen, onOpen, onClose } = useDisclosure()
 	const btnRef: RefObject<any> = useRef()
 	const [visLoading, setVisLoading] = useState(false)
+	const [full, setFull] = useState<UserArray>([])
+	const [edit, setEdit] = useState<UserArray>([])
+	const [view, setView] = useState<UserArray>([])
+
 	const [visRes, setVis] = useMutation<{}, SetVisibilityArgsVars>(SetVisibilityQuery)
+	const [userPermissionResponse, getUserPermissions] = useQuery<GetUserPermissionResult, GetUserPerissionsVars>({
+		query: GetUserPermissions,
+		variables: { blockId: blockId },
+	})
+
+	useEffect(() => {
+		getUserPermissions()
+	}, [])
+
+	useEffect(() => {
+		if (userPermissionResponse.data?.blockById) {
+			const permissions = userPermissionResponse.data?.blockById
+			setFull(permissions.full)
+			setEdit(permissions.edit)
+			setView(permissions.view)
+		}
+	}, [userPermissionResponse])
+
+	const renderUserItems = (users: UserArray, type: PermissionType) => {
+		const results = users.map((user, i) => {
+			const displayName = user.displayName || user.username
+			const content = (
+				<Flex alignItems='center'>
+					<Avatar size='sm' name={displayName} />
+					<HStack width='100%' alignItems='flex-end' ml={2}>
+						<Stack width='100%' spacing={0}>
+							<Text fontWeight='bold'>{displayName}</Text>
+							<Text>@{user.username}</Text>
+						</Stack>
+						<Box width='70%' justifyContent='flex-end'>
+							<Select
+								value={type}
+								onChange={(e: any) => {
+									const index = e.target.value
+									const destination = index == 0 ? full : index == 1 ? edit : index == 2 ? view : undefined
+									if (!destination) {
+										return
+									}
+
+									const setDestination = index == 0 ? setFull : index == 1 ? setEdit : setView
+									const exists = destination.some(e => e.username === user.username)
+									if (!exists) {
+										setView(oldValue => oldValue.filter(e => e.username !== user.username))
+										setEdit(oldValue => oldValue.filter(e => e.username !== user.username))
+										setFull(oldValue => oldValue.filter(e => e.username !== user.username))
+										setDestination(oldValue => [...oldValue, user])
+									}
+								}}>
+								<option value={0}>Full</option>
+								<option value={1}>Edit</option>
+								<option value={2}>Read-Only</option>
+							</Select>
+						</Box>
+					</HStack>
+				</Flex>
+			)
+
+			return (
+				<Box
+					key={user.username}
+					cursor='pointer'
+					px={4}
+					py={1}
+					onClick={() => {
+
+					}}
+				>
+					{content}
+				</Box>
+			)
+		})
+
+		return results
+	}
+
+	const renderUserList = () => {
+		return <>
+			{full.length > 0 &&
+				<Stack maxH='80vh' overflow='scroll' spacing={0}>
+					<Heading size='sm' fontWeight='semibold' marginTop='10'>Full</Heading>
+					{renderUserItems(full, PermissionType.full)}
+				</Stack>}
+			{edit.length > 0 &&
+				<Stack maxH='80vh' overflow='scroll' spacing={0}>
+					<Heading size='sm' fontWeight='semibold' marginTop='10'>Edit</Heading>
+					{renderUserItems(edit, PermissionType.edit)}
+				</Stack>}
+			{view.length > 0 &&
+				<Stack maxH='80vh' overflow='scroll' spacing={0}>
+					<Heading size='sm' fontWeight='semibold' marginTop='10'>Read-Only</Heading>
+					{renderUserItems(view, PermissionType.view)}
+				</Stack>}
+		</>
+	}
 
 	const drawer = (
-		<Drawer isOpen={isOpen} placement='right' onClose={onClose} finalFocusRef={btnRef}>
+		<Drawer size='sm' isOpen={isOpen} placement='right' onClose={onClose} finalFocusRef={btnRef}>
 			<DrawerOverlay>
 				<DrawerContent>
 					<DrawerCloseButton />
 					<DrawerHeader>Block Permissions</DrawerHeader>
-
 					<DrawerBody>
 						<Flex width='100%' justifyContent='space-between'>
 							<Flex alignItems='center'>
@@ -64,6 +216,27 @@ export const usePermissionButton = (blockId: number, pub: boolean): [JSX.Element
 							/>
 						</Flex>
 						<Text color='red'>{visRes.error?.message}</Text>
+
+						<HStack alignItems='flex-end' marginTop='10'>
+							<Heading width='100%' size=' md' fontWeight='semibold'>User Permissions</Heading>
+							<SearchComponentWrapper component={{ cid: 'search', type: 'User' }}>
+								<Button
+									justifyContent='flex-end'
+									colorScheme='orange'
+									variant='link'>
+									Add
+								</Button>
+							</SearchComponentWrapper>
+							<Button
+								onClick={() => { }}
+								justifyContent='flex-end'
+								colorScheme='orange'
+								variant='link'>
+								Save
+							</Button>
+						</HStack>
+
+						{userPermissionResponse && renderUserList()}
 					</DrawerBody>
 
 					<DrawerFooter>
@@ -73,7 +246,7 @@ export const usePermissionButton = (blockId: number, pub: boolean): [JSX.Element
 					</DrawerFooter>
 				</DrawerContent>
 			</DrawerOverlay>
-		</Drawer>
+		</Drawer >
 	)
 	return [drawer, onOpen, btnRef]
 }
