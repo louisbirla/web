75b39091fd22f6fd081522e14bab625856e2c7b9
Louis Birla <louis.birla@gmail.com>
2021-03-28 11:57:29 -0500

    fix: styling on custom menu

--- components/display/components/menu/CustomMenu.tsx
+++ components/display/components/menu/CustomMenu.tsx
@@ -1,4 +1,5 @@
 import { IconButton } from "@chakra-ui/button"
+import { Box } from "@chakra-ui/layout"
 import { Tooltip } from "@chakra-ui/tooltip"
 import { CustomMenuItem } from "display-api"
 import { genActionObject } from "../../ActionObject"
@@ -14,20 +15,22 @@ export const CustomMenu: React.FC<{ customMenu: CustomMenuItem[]; color?: string
 			{customMenu.map(({ icon, text, interact, disabled }: CustomMenuItem) => {
 				const [ActionWrap, action] = genActionObject(interact)
 				return (
-					<ActionWrap key={text}>
-						<Tooltip label={text} hasArrow>
-							<IconButton
-								onClick={action}
-								size='xs'
-								variant='nostyle'
-								color={color}
-								disabled={disabled}
-								aria-label={text}
-								mr={margin ? 2 : undefined}
-								icon={<IconComponent color='none' name={icon} size={20} />}
-							/>
-						</Tooltip>
-					</ActionWrap>
+					<Tooltip label={text} hasArrow>
+						<Box display='inline-block'>
+							<ActionWrap key={text}>
+								<IconButton
+									onClick={action}
+									size='xs'
+									variant='nostyle'
+									color={color}
+									disabled={disabled}
+									aria-label={text}
+									mr={margin ? 2 : undefined}
+									icon={<IconComponent color='none' name={icon} size={20} />}
+								/>
+							</ActionWrap>
+						</Box>
+					</Tooltip>
 				)
 			})}
 		</>
