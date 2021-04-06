f6007ee589a19078cd4aa8a2b78993a9fa706709
Salman Maredia <salmanaly1492@gmail.com>
2021-04-06 10:09:47 -0400

    Moved on_change out of interact property and set the variable

--- components/display/components/StickyToggleButton.tsx
+++ components/display/components/StickyToggleButton.tsx
@@ -1,5 +1,7 @@
 import { StickyToggleButtonArgs } from "display-api"
 import { useState } from "react"
+import { genActionObject } from "../ActionObject"
+import { setMethodVar } from "../method"
 import { ButtonComponent } from "./Button"
 
 export const StickyToggleButtonComponent: React.FC<StickyToggleButtonArgs> = ({
@@ -8,23 +10,27 @@ export const StickyToggleButtonComponent: React.FC<StickyToggleButtonArgs> = ({
 	on_change,
 	default_value,
 }) => {
+	const [ActionWrap, action] = genActionObject(on_change)
 	const [isEnabled, setEnabled] = useState<boolean>(default_value ?? true)
 
 	const onChange = () => {
 		setEnabled(!isEnabled)
+		name && setMethodVar(name, isEnabled)
+		action()
 	}
 
 	return (
-		<ButtonComponent
-			text={name ? name : button.text}
-			variant={button.variant}
-			icon={button.icon}
-			size={button.size}
-			color_scheme={isEnabled ? button.color_scheme : "gray"}
-			disabled={button.disabled}
-			readonly={true}
-			interact={on_change}
-			onChange={onChange}
-		/>
+		<ActionWrap>
+			<ButtonComponent
+				text={button.text}
+				variant={button.variant}
+				icon={button.icon}
+				size={button.size}
+				color_scheme={isEnabled ? button.color_scheme : "gray"}
+				disabled={button.disabled}
+				readonly={true}
+				onChange={onChange}
+			/>
+		</ActionWrap>
 	)
 }
