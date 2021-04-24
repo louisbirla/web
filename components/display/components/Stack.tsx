baf231801cc57eb837d9b18d409710484492a919
Louis Birla <louis.birla@gmail.com>
2021-04-24 16:20:21 -0500

    fix: add aligning

--- components/display/components/Stack.tsx
+++ components/display/components/Stack.tsx
@@ -2,15 +2,21 @@ import { Box, Flex, HStack, Stack } from "@chakra-ui/react"
 import { StackArgs } from "display-api"
 import { ComponentDelegate, Environment } from "../ComponentDelegate"
 
-export const StackComponent: React.FC<StackArgs & { env?: Environment }> = ({ direction = "Fit", items, env }) => {
+export const StackComponent: React.FC<StackArgs & { env?: Environment }> = ({
+	direction = "Fit",
+	items,
+	env,
+	align_x,
+	align_y,
+}) => {
 	const content = items.map(({ component }, i) => (
-		<Box key={`${i}${component.cid}`}>
-			<ComponentDelegate env={env} key={JSON.stringify(component)} component={component} />
+		<Box m={1} key={`${i}${component.cid}`} display='inline-block'>
+			<ComponentDelegate env={env} component={component} />
 		</Box>
 	))
-	switch (direction) {
+	switch (direction ?? "Fit") {
 		case "Horizontal":
-			return <HStack>{content}</HStack>
+			return <HStack alignItems={flexLang(align_y)}>{content}</HStack>
 		case "Fit":
 			return (
 				<Flex flexWrap='wrap' justifyItems='flex-start'>
@@ -18,6 +24,16 @@ export const StackComponent: React.FC<StackArgs & { env?: Environment }> = ({ di
 				</Flex>
 			)
 		default:
-			return <Stack>{content}</Stack>
+			return <Stack alignItems={flexLang(align_x)}>{content}</Stack>
 	}
+}
+
+function flexLang(s?: string) {
+	if (s == "Middle") {
+		return "center"
+	}
+	if (s == "Bottom" || s == "Right") {
+		return "flex-end"
+	}
+	return "flex-start"
 }
