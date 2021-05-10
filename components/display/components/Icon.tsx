4284b212279bb15b2f3ebc0d05bcdacf845f052f
Louis Birla <louis.birla@gmail.com>
2021-05-10 11:02:59 -0500

    fix: support for many more icons

--- components/display/components/Icon.tsx
+++ components/display/components/Icon.tsx
@@ -1,15 +1,40 @@
 import { IconName } from "display-api"
+import { Trash, Unlock } from "react-feather"
 import {
+	Anchor,
+	Archive,
+	Award,
+	Book,
 	Box as BoxIcon,
 	CheckCircle,
 	Folder,
 	IconProps,
 	MessageSquare,
 	Plus,
-	Rss,
 	ThumbsDown,
 	ThumbsUp,
 	Type,
+	Bookmark,
+	Briefcase,
+	Calendar,
+	Edit,
+	Eye,
+	Inbox,
+	File,
+	FileText,
+	Film,
+	Filter,
+	Flag,
+	Gift,
+	Heart,
+	Image,
+	Info,
+	Key,
+	Lock,
+	Map,
+	MapPin,
+	Minus,
+	Send,
 } from "react-feather"
 
 export const IconComponent: React.FC<{ name?: IconName; icon?: IconName; color?: string; size?: number | string }> = ({
@@ -25,24 +50,76 @@ export const IconComponent: React.FC<{ name?: IconName; icon?: IconName; color?:
 	}
 	let commonProps: IconProps = { color, size: size ?? 23 }
 	switch (name ?? icon) {
-		case "Folder":
-			return <Folder {...commonProps} />
-		case "TaskComplete":
-			return <CheckCircle {...commonProps} />
-		case "Message":
-			return <MessageSquare {...commonProps} style={{ marginTop: "4px" }} />
+		case "Anchor":
+			return <Anchor {...commonProps} />
+		case "Archive":
+			return <Archive {...commonProps} />
+		case "Award":
+			return <Award {...commonProps} />
+		case "Book":
+			return <Book {...commonProps} />
+		case "Bookmark":
+			return <Bookmark {...commonProps} />
 		case "Box":
 			return <BoxIcon {...commonProps} />
-		case "Type":
-			return <Type {...commonProps} />
+		case "Briefcase":
+			return <Briefcase {...commonProps} />
+		case "Calendar":
+			return <Calendar {...commonProps} />
+		case "Edit":
+			return <Edit {...commonProps} />
+		case "Eye":
+			return <Eye {...commonProps} />
 		case "Feed":
-			return <Rss {...commonProps} />
+			return <Inbox {...commonProps} />
+		case "File":
+			return <File {...commonProps} />
+		case "FileText":
+			return <FileText {...commonProps} />
+		case "Film":
+			return <Film {...commonProps} />
+		case "Filter":
+			return <Filter {...commonProps} />
+		case "Flag":
+			return <Flag {...commonProps} />
+		case "Folder":
+			return <Folder {...commonProps} />
+		case "Gift":
+			return <Gift {...commonProps} />
+		case "Heart":
+			return <Heart {...commonProps} />
+		case "Image":
+			return <Image {...commonProps} />
+		case "Info":
+			return <Info {...commonProps} />
+		case "Key":
+			return <Key {...commonProps} />
+		case "Lock":
+			return <Lock {...commonProps} />
+		case "Map":
+			return <Map {...commonProps} />
+		case "MapPin":
+			return <MapPin {...commonProps} />
+		case "Message":
+			return <MessageSquare {...commonProps} style={{ marginTop: "4px" }} />
+		case "Minus":
+			return <Minus {...commonProps} />
 		case "Plus":
 			return <Plus {...commonProps} />
-		case "ThumbsUp":
-			return <ThumbsUp {...commonProps} />
+		case "Send":
+			return <Send {...commonProps} />
+		case "TaskComplete":
+			return <CheckCircle {...commonProps} />
 		case "ThumbsDown":
 			return <ThumbsDown {...commonProps} />
+		case "ThumbsUp":
+			return <ThumbsUp {...commonProps} />
+		case "Trash":
+			return <Trash {...commonProps} />
+		case "Type":
+			return <Type {...commonProps} />
+		case "Unlock":
+			return <Unlock {...commonProps} />
 		default:
 			return <BoxIcon {...commonProps} />
 	}
